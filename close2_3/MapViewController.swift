99f2bfa7955f5ae72190d372688b149e15c96f27
Vasilkov2022 <vasilkovandr249@gmail.com>
2023-01-15 16:21:16 +0300

    Google maps are installed

---
+++ close2_3/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  close2_3
+//
+//  Created by Андрей Васильков on 15.01.2023.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
