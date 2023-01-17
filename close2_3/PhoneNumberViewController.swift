a707b133efad5b6154911225d50f702b57e364bf
zimble111 <gnilomedovpv@gmail.com>
2023-01-17 23:40:49 +0300

    Counry code added

---
+++ close2_3/PhoneNumberViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class PhoneNumberViewController: UIViewController {
+    @IBOutlet weak var countryCode: UIButton!
+    @IBOutlet weak var phoneNumber: UITextField!
+    
+    private let maxNumberCount = 11
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+}
